e202add2a48d0f2cef026aac9e78873b9b6527e8
David Turner <susbdt@gmail.com>
2016-06-11 22:39:56 -0700

    X

--- markdown-website/main.js
+++ markdown-website/main.js
@@ -1,9 +1,15 @@
 // This code derived from: 
 // http://stackoverflow.com/questions/950087/include-a-javascript-file-in-another-javascript-file
 (function() {
+	function buildLinks(text) {
+		// [[Hello]] -> [Hello](hello.html)
+		// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+		return text.replace(/\[\[([A-Za-z0-9_]+)\]\]/g, "[$1]($1.html)");
+	}
 	function onload() {
 		var preElement = document.getElementsByTagName('pre')[0];
-		document.body.innerHTML = marked(preElement.innerHTML);
+		var markDownText = buildLinks(preElement.innerHTML);
+		document.body.innerHTML = marked(markDownText);
 	}
 	var head = document.getElementsByTagName('head')[0];
 	var script = document.createElement('script');
